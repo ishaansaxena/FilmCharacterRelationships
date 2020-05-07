e38345602115343b429d8831f62ba39c2436f003
Ishaan <isaxena@purdue.edu>
2020-05-07 14:15:27 -0400

    add w conditions

--- extractMetadataForRMN.py
+++ extractMetadataForRMN.py
@@ -16,21 +16,34 @@ with open("data/glove/glove.6B.300d.txt", 'r') as f:
         word = values[0]
         vector = np.asarray(values[1:], "float32")
         embeddings_dict[word] = vector
-    
+   
+
+def w_condition(w):
+    return (w not in stopset) and (w in embeddings_dict) and (len(w) > 2)
+
 We = []
 wmap = {}
+wcnt = {}
+wthresh = 2
 with open('data/cdmn_mds/movie_lines.txt', encoding='ISO-8859-1') as f:
     ldb  = list(map(lambda x: x.split(' +++$+++ ')[-1], f.read().split('\n')))
     # tkns = word_tokenize(" ".join(ldb).lower())
     tkns = tokenizer.tokenize(" ".join(ldb).lower())
-    tkns = [w for w in tkns if w not in stopset and w in embeddings_dict]
+    tkns = [w for w in tkns if w_condition(w)]
     for t in tkns:
-        if t not in wmap:
-            widx = len(wmap)
-            wmap[t] = widx
-            We.append(embeddings_dict[t])
-# with open('rmn/data/_metadata.pkl', 'rb') as f:
-#     wmap, _, _ = pickle.load(f)
+        if t not in wcnt:
+            wcnt[t] = 0
+        else:
+            wcnt[t] += 1
+
+wcntl = sorted(wcnt.keys(), key=wcnt.get, reverse=True)
+for i in range(len((wcntl))):
+    tok = wcntl[i]
+    if wcnt[tok] > wthresh:
+        We.append(embeddings_dict[tok])
+        wmap[tok] = len(wmap)
+
+We = np.array(We)
     
 with open('data/cdmn_mds/movie_characters_metadata.txt', encoding='ISO-8859-1') as f:
     cdb  = list(map(lambda x: x.split(' +++$+++ '), f.read().split('\n')))
@@ -43,4 +56,4 @@ with open('rmndata/metadata.pkl', 'wb') as f:
     pickle.dump((wmap, cmap, bmap), f, protocol=2)
 
 with open('rmndata/glove.We', 'wb') as f:
-    pickle.dump(np.array(We), f, protocol=2)
+    pickle.dump(We, f, protocol=2)
